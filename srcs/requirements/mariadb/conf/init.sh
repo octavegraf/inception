2ff3c633815181ab79a8940a5efff33742a022a5
Octave Graf <cobra.morilles-0n@icloud.com>
2026-03-10 14:56:01 +0100

    refactor mariadb setup: move environment variables to docker-compose and enhance init script for database initialization

--- srcs/requirements/mariadb/conf/init.sh
+++ srcs/requirements/mariadb/conf/init.sh
@@ -1,11 +1,27 @@
-#!bin!bash
+#!/bin/bash
+set -x
 
-service mysql start;
-mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
-mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-exec mysqld_safe
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
+chown -R mysql:mysql /var/lib/mysql
+
+mysql_install_db --user=mysql --datadir=/var/lib/mysql --skip-test-db
+chown -R mysql:mysql /var/lib/mysql
+
+mysqld --user=mysql --skip-networking &
+
+while ! mysqladmin ping --silent 2>/dev/null; do
+	sleep 1
+done
+
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+mysql -u root -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${SQL_USER}'@'%';"
+mysql -u root -e "FLUSH PRIVILEGES;"
+
+touch /var/lib/mysql/.inception_init
+
+mysqladmin -u root shutdown
+sleep 2
+
+exec mysqld --user=mysql
