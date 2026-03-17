a58e77674b90969195e64e8552f6d950bf2bf174
Octave Graf <32437368+octavegraf@users.noreply.github.com>
2026-03-17 17:00:01 +0100

    refactor WordPress configuration: update domain variable, enhance init script for database connection, and replace placeholders in wp-config.php

--- srcs/requirements/wordpress/conf/init.sh
+++ srcs/requirements/wordpress/conf/init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -x
 
+until (echo > /dev/tcp/mariadb/3306) do
+  sleep 2
+done
+
 # Copy conf if not exist
 test -f /var/www/wordpress/wp-config.php || cp -r /usr/src/wordpress/* /var/www/wordpress/
 cp /usr/src/wordpress/wp-config.php /var/www/wordpress/
@@ -17,10 +21,23 @@ LOGGED_IN_SALT=$(cat /run/secrets/logged_in_salt)
 NONCE_SALT=$(cat /run/secrets/nonce_salt)
 WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
 
+sed -i "s#DB_NAME_PLACEHOLDER#${DB_NAME}#g" /var/www/wordpress/wp-config.php
+sed -i "s#DB_USER_PLACEHOLDER#${DB_USER}#g" /var/www/wordpress/wp-config.php
+sed -i "s#DB_PASSWORD_PLACEHOLDER#${DB_PASSWORD}#g" /var/www/wordpress/wp-config.php
+sed -i "s#AUTH_KEY_PLACEHOLDER#${AUTH_KEY}#g" /var/www/wordpress/wp-config.php
+sed -i "s#SECURE_AUTH_KEY_PLACEHOLDER#${SECURE_AUTH_KEY}#g" /var/www/wordpress/wp-config.php
+sed -i "s#LOGGED_IN_KEY_PLACEHOLDER#${LOGGED_IN_KEY}#g" /var/www/wordpress/wp-config.php
+sed -i "s#NONCE_KEY_PLACEHOLDER#${NONCE_KEY}#g" /var/www/wordpress/wp-config.php
+sed -i "s#AUTH_SALT_PLACEHOLDER#${AUTH_SALT}#g" /var/www/wordpress/wp-config.php
+sed -i "s#SECURE_AUTH_SALT_PLACEHOLDER#${SECURE_AUTH_SALT}#g" /var/www/wordpress/wp-config.php
+sed -i "s#LOGGED_IN_SALT_PLACEHOLDER#${LOGGED_IN_SALT}#g" /var/www/wordpress/wp-config.php
+sed -i "s#NONCE_SALT_PLACEHOLDER#${NONCE_SALT}#g" /var/www/wordpress/wp-config.php
+sed -i "s#WP_HOME_PLACEHOLDER#${DOMAIN}#g" /var/www/wordpress/wp-config.php
+sed -i "s#WP_SITEURL_PLACEHOLDER#${DOMAIN}#g" /var/www/wordpress/wp-config.php
+
 cd /var/www/wordpress
 
-wp config create --allow-root --dbname="${DB_NAME}" --dbuser="${DB_USER}" --dbpass="${DB_PASSWORD}" --dbhost="${DB_HOST}" --path="/var/www/wordpress"
-wp core install --allow-root --url="https://ocgraf.42.fr" --title="${WP_SITE_TITLE}" --admin_user="${WP_ADMIN_USERNAME}" --admin_password="${WP_ADMIN_PASSWORD}" --admin_email="${WP_ADMIN_EMAIL}" --path="/var/www/wordpress"
+wp core install --allow-root --url="${DOMAIN}" --title="${WP_SITE_TITLE}" --admin_user="${WP_ADMIN_USERNAME}" --admin_password="${WP_ADMIN_PASSWORD}" --admin_email="${WP_ADMIN_EMAIL}" --path="/var/www/wordpress"
 
 sleep 10
 exec php-fpm8.2 -F
